0f0e926e77426651e9e7061328028d470388e942
Matthew Velasco <MVelasco90@hotmail.com>
2021-07-25 20:36:04 -0700

    C## basics mixed with Unity basics complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 5;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,14 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+
+        transform.position = pos;
     }
 }
